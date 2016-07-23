f41909dfcbaabdab15fce658b92a23a88e4854cb
rudy <rmolerof@gmail.com>
2016-07-23 13:38:47 -0400

    minor tweaks

--- src/main/webapp/resources/js/controllers/Class1Controller.js
+++ src/main/webapp/resources/js/controllers/Class1Controller.js
@@ -9,7 +9,7 @@ var Class1Controller = function($scope, $http) {
 	var gem = {
 			name: 'Dodecahedron',
 			price: 2.95,
-			description: 'Some gems have hidden qualities beyond the luster, beyond their shine, Dodeca is one of those gems',
+			description: 'Some Gems have hidden qualities beyond the luster, beyond their shine, Dodeca is one of those gems',
 			canPurchase: true,
 			soldOut: false
 	};
